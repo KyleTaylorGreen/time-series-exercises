d06e09fcfff2449527d70c9126a16e97f396f14b
KyleTaylorGreen <kyle.taylor.green@gmail.com>
2022-06-22 14:37:03 -0500

    clarifying comments

--- async_acquire.py
+++ async_acquire.py
@@ -31,6 +31,7 @@ async def worker(s, url):
 # through list of urls to to get responses.
 async def main(url_list):
     s = asks.Session(connections=200)
+    # context manager to wait async functions
     async with trio.open_nursery() as nursery:
         for url in url_list:
             nursery.start_soon(worker,s, url)
@@ -43,20 +44,28 @@ def request_all_urls_comparison():
 
     #urls = [[i, 'http://serebii.net'] for i in range(150)]
     start = time.perf_counter()
-    connections=200
-    retries=10
-    print('hello')
+
+    # runs main async function
     trio.run(main, urls)
     
+    # get end time of async function
+    # start time of sync function
     async_end = time.perf_counter() - start
-    
     sync_start = time.perf_counter()
+    
+    # start sync function
+    # calculate time to complete sync function
     sequential = acquire.all_resource_dfs(['items', 'sales', 'stores'])
     sync_end = time.perf_counter() - sync_start
 
+    # display the times for each
     print(f"Async time to complete: {async_end}")
     print(f'synchronous time to end: {sync_end}')
 
 
 if __name__ == '__main__':
     request_all_urls_comparison()
+
+    # first results
+    # async: ~140 seconds
+    # sync: ~220 seconds
